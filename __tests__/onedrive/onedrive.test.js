2688ded23f151d1a8e41b9d845e98e861c192d50
Minghe <h.minghe@gmail.com>
2019-09-24 12:25:40 +0800

    add more api (#6)

--- __tests__/onedrive/onedrive.test.js
+++ __tests__/onedrive/onedrive.test.js
@@ -21,6 +21,20 @@ describe('onedrive workflow', () => {
     res = await api.upload({ dest, content})
     expect(res.ok).toBeTruthy()
   }, 15000)
-  test('list', async () => {})
-  test('download', async () => {})
+
+  test('get item', async () => {
+    let res = await api.getItem('test')
+    expect(res.ok).toBeTruthy()
+  })
+  test('list', async () => {
+    const res = await api.list(dir)
+    expect(res.ok).toBeTruthy()
+  }, 15000)
+  test('download', async () => {
+    // FIXME it's weird, it will report a Cross oirig null error
+    // let res = await api.download('test/hello.txt')
+    // expect(res.ok).toBeTruthy()
+    // res = await api.download('test/hello.jpeg')
+    // expect(res.ok).toBeTruthy()
+  }, 15000)
 })
